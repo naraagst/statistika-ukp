f5004b596073e46eb6c2ac89c97ce9549b3d3f91
naraagst <naraagst12@gmail.com>
2020-08-26 12:44:44 +0800

    mobile interface fixed

--- Assets/js/mean3.js
+++ Assets/js/mean3.js
@@ -43,6 +43,8 @@ function consomean(){
 	document.getElementById("containermodus").hidden = false;
 	document.getElementById("cmean1").style="border:5px solid rgb(0,156,255, 0.5);border-bottom: none;background-color: white;";
 	document.getElementById("cmean2").style = "background-color:rgb(0,156,255, 0.5);border:1px solid white;";
+	document.getElementById("cmean1a").style="border:2px solid rgb(0,156,255, 0.5);border-bottom: none;background-color: white;";
+	document.getElementById("cmean2a").style = "background-color:rgb(0,156,255, 0.5);border:1px solid white;";
 	document.getElementById("consomodus").hidden=false;
 	document.getElementById("cobamodus").hidden=true;
 }
@@ -50,6 +52,8 @@ function cobamean(){
 	document.getElementById("containermodus").hidden = false;
 	document.getElementById("cmean2").style="border:5px solid rgb(0,156,255, 0.5);border-bottom: none;background-color: white;";
 	document.getElementById("cmean1").style = "background-color:rgb(0,156,255, 0.5);border:1px solid white;";
+	document.getElementById("cmean2a").style="border:2px solid rgb(0,156,255, 0.5);border-bottom: none;background-color: white;";
+	document.getElementById("cmean1a").style = "background-color:rgb(0,156,255, 0.5);border:1px solid white;";
 	document.getElementById("cobamodus").hidden=false;
 	document.getElementById("consomodus").hidden=true;
 }
