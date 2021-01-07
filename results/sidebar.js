9c6627cc035073a801dd85f76eba3037f2dacf1f
naraagst <naraagst12@gmail.com>
2021-01-07 23:21:27 +0800

    update final

--- results/sidebar.js
+++ results/sidebar.js
@@ -4,10 +4,10 @@ const getNavLinkClass = (path) => {
 
 document.querySelector(".sidebar").innerHTML = `
 	<ul>
-		<li><a href="jwbkuisa.html" class="${getNavLinkClass("kuisa.html")}">1. Kuis A</a></li>
-		<li><a href="jwbkuisb.html" class="${getNavLinkClass("kuisb.html")}">2. Kuis B</a></li>
-		<li><a href="jwbkuisc.html" class="${getNavLinkClass("kuisc.html")}">3. Kuis C</a></li>
-		<li><a href="jwbposttest.html" class="${getNavLinkClass("posttest.html")}">4. Latihan</a></li>
+		<li><a href="kuisa.html" class="${getNavLinkClass("kuisa.html")}">1. Kuis A</a></li>
+		<li><a href="kuisb.html" class="${getNavLinkClass("kuisb.html")}">2. Kuis B</a></li>
+		<li><a href="kuisc.html" class="${getNavLinkClass("kuisc.html")}">3. Kuis C</a></li>
+		<li><a href="latihan.html" class="${getNavLinkClass("latihan.html")}">4. Latihan</a></li>
 	</ul>
 `
 function sidebar() {
